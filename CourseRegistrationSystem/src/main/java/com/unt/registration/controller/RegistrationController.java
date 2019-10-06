3510bfec4563adf6af87f7410d3f0510f47a7ac1
sreekanthv6 <sreekanthkumarvobilishetty@my.unt.edu>
2019-10-06 15:30:15 -0500

    Login and student index pages

--- CourseRegistrationSystem/src/main/java/com/unt/registration/controller/RegistrationController.java
+++ CourseRegistrationSystem/src/main/java/com/unt/registration/controller/RegistrationController.java
@@ -1,26 +1,46 @@
 package com.unt.registration.controller;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
 import com.unt.registration.service.RegistrationServiceImpl;
 import com.unt.registration.util.User;
 
 @RestController
-@RequestMapping("/RegistrationController")
+//@RequestMapping("/RegistrationController")
 public class RegistrationController {
-	
-	@Autowired
-	RegistrationServiceImpl registrationServiceImpl;
-	
-	@GetMapping(path="/getUserDetails", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
-public User getUserDetails() {
-	User user= registrationServiceImpl.getUserDetails();
-	return user;
-	
-	
-}
+	/*
+	 * public static void main(String[] args) {
+	 * SpringApplication.run(RegistrationController.class, args); }
+	 */
+    @GetMapping("/login")
+    public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	System.out.println(request.getParameter("uname"));
+		RequestDispatcher rd=request.getRequestDispatcher("student_index.html");
+		rd.forward(request, response);
+    }
+		
+	@GetMapping("/add")
+	public void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd=request.getRequestDispatcher("select_degree.jsp");
+		rd.include(request, response);
+		
+    	
+    	
+    }
+
 }
---
+++ CourseRegistrationSystem/src/main/resources/static/select_degree.jsp
@@ -0,0 +1,35 @@
+<html>
+<title> Select Degree </title>
+<body>
+<div>
+<script src="Menu_Common.js">
+</script>
+</div>
+<br>
+<br>
+<br>
+
+<div class="degree"> 
+<form action="#" name="add">
+Degree:
+<select name="degree">
+  <option value="UG">Under Graduate</option>
+  <option value="G">Graduate</option>
+
+</select><br><br>
+Course:
+<select name="course">
+  <option value="CS">Computer Science</option>
+  <option value="ME">Mechanical</option>
+  <option value="EE">Electrical</option>
+
+</select>
+<br>
+
+<button name=button type=submit value="submit"> Submit </button>
+</form>
+</div>
+
+
+</body>
+</html>
